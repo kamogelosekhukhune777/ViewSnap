ef1a026a75712b15ae7802dff41ac0bf8035a217
Kamogelo Sekhukhune <kamogelosekhukhune01@gmail.com>
2023-10-27 20:45:19 +0200

    added and applied a navbar

--- views/view.go
+++ views/view.go
@@ -11,6 +11,7 @@ func NewView(Layout string, files ...string) *View {
 	files = append(files,
 		"views/layouts/footer.html",
 		"views/layouts/bootstrap.html",
+		"views/layouts/navbar.html",
 	)
 
 	t, err := template.ParseFiles(files...)
