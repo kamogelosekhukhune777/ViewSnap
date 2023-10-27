5d3f5c5cfd7895d6aba3a86d793470a7d61289a2
Kamogelo Sekhukhune <kamogelosekhukhune01@gmail.com>
2023-10-27 22:48:43 +0200

    globbing our templates files

--- views/view.go
+++ views/view.go
@@ -1,6 +1,14 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".html"
+)
 
 type View struct {
 	Template *template.Template
@@ -8,11 +16,7 @@ type View struct {
 }
 
 func NewView(Layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.html",
-		"views/layouts/bootstrap.html",
-		"views/layouts/navbar.html",
-	)
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -22,4 +26,14 @@ func NewView(Layout string, files ...string) *View {
 		Template: t,
 		Layout:   Layout,
 	}
+}
+
+// layoutFiles return a slice of string representing
+// the layout files used in our web app
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + ".html")
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
